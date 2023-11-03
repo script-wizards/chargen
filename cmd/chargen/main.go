32107ea23c99aaa92ce0700bd3882171cdcd7dd1
Panat Taranat <panat.taranat@gmail.com>
2023-11-03 16:02:24 -0400

    add 404 page

--- cmd/chargen/main.go
+++ cmd/chargen/main.go
@@ -37,6 +37,18 @@ func main() {
 			return
 		}
 	})
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("templates/404.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
 
